69767f30ad5c87499382d4c7dffe8dbf79755ad5
Oleg Dylevich <oleg.dylevich@gmail.com>
2020-06-06 18:03:09 +0300

    Added bot

---
+++ classes.py
@@ -0,0 +1,127 @@
+import datetime
+from datetime import timedelta
+import pandas as pd
+
+
+min_plank_time = 30
+users_db_path = 'D:/Python projects/PlankBot/users_db.h5'
+logs_db_path = 'D:/Python projects/PlankBot/logs_db.h5'
+
+
+class User:
+    def __init__(self, user_id, chat_id, name=None):
+        self.name = name
+        self.user_id = user_id
+        self.chat_id = chat_id
+        self.current_time = min_plank_time
+        self.time_increase = 15
+        self.increase_in_days = 14
+        self.increase_day = datetime.datetime.today().date() + timedelta(days=14)
+        self.times_missed = 0
+        self.planked_today = False
+
+    def load(self):
+        users = pd.read_hdf(users_db_path, key='df')
+        print(users)
+        user_line = users.loc[(users['user_id'] == self.user_id) & (users['chat_id'] == self.chat_id)]
+        if user_line.shape[0] > 0:
+            self.current_time = user_line['min_plank_time'].iloc[0]
+            self.times_missed = user_line['times_missed'].iloc[0]
+            self.name = user_line['name'].iloc[0]
+            self.time_increase = user_line['time_increase'].iloc[0]
+            self.increase_in_days = user_line['increase_in_days'].iloc[0]
+            self.increase_day = user_line['increase_day'].iloc[0]
+            print('Loaded existing user')
+            self.describe()
+        else:
+            print('No such user exists. Creating new one')
+            self.write()
+
+    def write(self):
+        users_df = pd.read_hdf(users_db_path, key='df')
+        '''if self.user_id not in users_df.user_id:'''
+        row = pd.Series({'chat_id': self.chat_id,
+                         'user_id': self.user_id,
+                         'name': self.name,
+                         'min_plank_time': self.current_time,
+                         'time_increase': self.time_increase,
+                         'increase_in_days': self.increase_in_days,
+                         'increase_day': self.increase_day,
+                         'times_missed': self.times_missed})
+        users_df = users_df.append(row, ignore_index=True)
+        users_df.to_hdf(users_db_path, key='df'
+)
+
+    def amend(self):
+        users_df = pd.read_hdf(users_db_path, key='df')
+        '''update all'''
+        users_df.loc[(users_df['user_id'] == self.user_id) & (users_df['chat_id'] == self.chat_id),
+                     'min_plank_time'] = self.current_time
+        users_df.loc[(users_df['user_id'] == self.user_id) & (users_df['chat_id'] == self.chat_id),
+                     'times_missed'] = self.times_missed
+        users_df.loc[(users_df['user_id'] == self.user_id) & (users_df['chat_id'] == self.chat_id),
+                     'name'] = self.name
+        users_df.loc[(users_df['user_id'] == self.user_id) & (users_df['chat_id'] == self.chat_id),
+                     'time_increase'] = self.time_increase
+        users_df.loc[(users_df['user_id'] == self.user_id) & (users_df['chat_id'] == self.chat_id),
+                     'increase_in_days'] = self.increase_in_days
+        users_df.loc[(users_df['user_id'] == self.user_id) & (users_df['chat_id'] == self.chat_id),
+                     'increase_day'] = self.increase_day
+        '''save'''
+        users_df.to_hdf(users_db_path, key='df')
+
+    def describe(self):
+        print('Chat ID:', self.chat_id)
+        print('User ID:', self.user_id)
+        print('Name:', self.name)
+        print('Current plank time:', self.current_time)
+        print('Time increase in seconds:', self.time_increase)
+        print('Increase every # of days:', self.increase_in_days)
+        print('Next increase:', self.increase_day)
+        print('Times missed:', self.times_missed)
+
+    def check_if_user_exists(self):
+        self.load()
+
+    def write_planked_today(self, date):
+        logs_df = pd.read_hdf(logs_db_path, key='df')
+        print(logs_df)
+        user_line = logs_df.loc[(logs_df['user_id'] == self.user_id) &
+                                (logs_df['chat_id'] == self.chat_id) &
+                                (logs_df['date'] == date)]
+        if user_line.shape[0] > 0:
+            print('User already planked on ' + date)
+        else:
+            row = pd.Series({'chat_id': self.chat_id,
+                             'user_id': self.user_id,
+                             'date': date})
+            logs_df = logs_df.append(row, ignore_index=True)
+            logs_df.to_hdf(logs_db_path, key='df')
+
+    def check_planked_today(self, date):
+        logs_df = pd.read_hdf(logs_db_path, key='df')
+        user_line = logs_df.loc[(logs_df['user_id'] == self.user_id) &
+                                (logs_df['chat_id'] == self.chat_id) &
+                                (logs_df['date'] == date)]
+        if user_line.shape[0] > 0:
+            self.planked_today = True
+
+    def change_increase_date(self, date=None):
+        if date is None:
+            print('Before changes ', self.increase_day)
+            self.increase_day = self.increase_day + timedelta(days=self.increase_in_days)
+            print('After changes ', self.increase_day)
+            self.amend()
+            self.describe()
+        else:
+            self.increase_day = date
+            self.amend()
+
+    def change_current_time(self, time=None):
+        if time is None:
+            self.current_time = int(self.current_time) + int(self.time_increase)
+            self.amend()
+            self.describe()
+        else:
+            self.current_time = int(time)
+            self.amend()
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import requests
 import telebot
-
+from telebot import types
+import datetime
+from datetime import timedelta
+import pandas as pd
+import schedule
+from threading import Thread
+from time import sleep
+from classes import User
 
 bot = telebot.TeleBot('1088985217:AAHXJF3KofCRj1rkgpRURce8pYl4Ow1Zlu8')
 
-#proxy
+# proxy
 login = 'olegdylevich'
-pwd = '	W1o7SqQ'
+pwd = 'W1o7SqQ'
 ip = '89.191.230.201'
 port = '65233'
 
@@ -15,10 +21,135 @@ telebot.apihelper.proxy = {
   'https': 'https://{}:{}@{}:{}'.format(login, pwd, ip, port)
 }
 
+users_db_path = 'D:/Python projects/PlankBot/users_db.h5'
+logs_db_path = 'D:/Python projects/PlankBot/logs_db.h5'
+
+
+def schedule_checker():
+    while True:
+        schedule.run_pending()
+        sleep(1)
+
+
+def send_daily_stats():
+    users_df = pd.read_hdf(users_db_path, key='df')
+    for chat in users_df['chat_id'].drop_duplicates():
+        message = 'Daily check list as of ' + \
+                  str((datetime.datetime.today().date() - timedelta(days=1)).strftime("%d %b %Y")) + ': \n '
+        for user in users_df.loc[users_df['chat_id'] == chat]['user_id']:
+            check_user = User(user_id=user, chat_id=chat)
+            check_user.check_if_user_exists()
+            check_user.check_planked_today((datetime.datetime.today().date() - timedelta(days=1)).strftime("%d %b %Y"))
+            message = message + str(check_user.name) + ' - ' + str(check_user.planked_today) +\
+                      ' ' + str(datetime.datetime.today().date().strftime("%d %b %Y")) + ' \n '
+
+        bot.send_message(chat, message)
+
+
+def check_increase_date():
+    users_df = pd.read_hdf(users_db_path, key='df')
+    for chat in users_df['chat_id'].drop_duplicates():
+        message = ''
+        for user in users_df.loc[users_df['chat_id'] == chat]['user_id']:
+            check_user = User(user_id=user, chat_id=chat)
+            check_user.check_if_user_exists()
+            if check_user.increase_day == datetime.datetime.today().date():
+                check_user.change_increase_date()
+                check_user.change_current_time()
+                message = message + check_user.name +\
+                        ' , congrats! Today is your increase day! Your new time is ' +\
+                        str(int(check_user.current_time)) + ' seconds! Your next' \
+                        ' increase date is ' +\
+                        str(check_user.increase_day.strftime("%d %b %Y"))
+                if message != '':
+                    bot.send_message(chat, message)
+
+
+if __name__ == "__main__":
+    schedule.every().day.at("05:00").do(send_daily_stats)
+    schedule.every().day.at("13:15").do(check_increase_date)
+    Thread(target=schedule_checker).start()
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Hi, plankers! I am very happy to greet you all here! I was created to help you'
+                                      ' track your progress. Right now I am a bit retarded and can not do much, but'
+                                      ' hopefully soon I will be able to remind you about mandatory time increases and'
+                                      ' track who planked each day and who missed, and other stuff like that. Please'
+                                      ' do not hesitate to write your suggestions to my creator. May the force to plank'
+                                      ' be always with you!')
 
 
-bot.polling()
+@bot.message_handler(commands=['set_min_plank_time'])
+def start_message(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add('Everybody', 'Me', 'Other user')
+    msg = bot.reply_to(message, 'Who do you want set up time for?', reply_markup=markup)
+    bot.register_next_step_handler(msg, find_users_to_set_up_time_to_step)
+
+
+def find_users_to_set_up_time_to_step(message):
+    if message.text == 'Everybody':
+        bot.send_message(message.chat.id, 'Ok, will set new time for everybody.')
+        reply_time = bot.reply_to(message, 'Please specify new time (in seconds) for ' + message.text + ':')
+    elif message.text == 'Me':
+        bot.send_message(message.chat.id, 'No problem mate, will adjust only your time.')
+    else:
+        get_user = bot.send_message(message.chat.id, 'Who is the poor fella you want to torture?')
+        bot.register_next_step_handler(get_user, set_up_time_step)
+
+
+def set_up_time_step(message):
+    print('you are in step 2')
+    for entity in message.entities:
+        print(entity)
+
+
+@bot.message_handler(content_types=['video'])
+def handle_docs_video(message):
+    user = User(user_id=message.from_user.id, chat_id=message.chat.id, name=message.from_user.first_name)
+    user.check_if_user_exists()
+
+    if int(datetime.datetime.fromtimestamp(message.date).strftime("%H")) > 5:
+        user.check_planked_today(datetime.datetime.fromtimestamp(message.date).strftime("%d %b %Y"))
+    else:
+        user.check_planked_today((datetime.datetime.fromtimestamp(message.date) - timedelta(days=1)).strftime("%d %b %Y"))
+
+    if user.planked_today is False:
+        if int(datetime.datetime.fromtimestamp(message.date).strftime("%H")) > 5:
+
+            if message.video.duration >= user.current_time:
+                bot.send_message(message.chat.id, 'Hooray! ' + message.from_user.first_name + ' planked on, '
+                             + str(datetime.datetime.fromtimestamp(message.date).strftime("%d %b %Y"))+'! Good job =)')
+                user.write_planked_today(datetime.datetime.fromtimestamp(message.date).strftime("%d %b %Y"))
+            else:
+                bot.send_message(message.chat.id,
+                                 'Dear ' + message.from_user.first_name + ', your current minimum planking time is '
+                                 + str(user.current_time) + ' seconds. Your video is ' + str(message.video.duration) +
+                                 ' seconds, which is less.')
+        else:
+
+            if message.video.duration >= user.current_time:
+                bot.send_message(message.chat.id, 'Hooray! ' + message.from_user.first_name + ' planked on, '
+                    + str((datetime.datetime.fromtimestamp(message.date) - timedelta(days=1)).strftime("%d %b %Y")) +
+                                 '! Good job =)')
+                user.write_planked_today((datetime.datetime.fromtimestamp(message.date) - timedelta(days=1)).strftime("%d %b %Y"))
+            else:
+                bot.send_message(message.chat.id,
+                                 'Dear ' + message.from_user.first_name + ', your current minimum planking time is '
+                                 + str(user.current_time) + ' seconds. Your video is ' + str(message.video.duration) +
+                                 ' seconds, which is less.')
+    else:
+        bot.send_message(message.chat.id,
+                         'Bro, you already planked today! You can stop)')
+
+
+while True:
+    try:
+        bot.polling()
+    except:
+        pass
+
+
+
